0e5c8254a66942c8cffd71b9db253aa40b02bb62
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 03:30:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@
   def hello(array)
   i = 0
   collection = []
-  while i < array.length
-  my_collect(collection) << yield(array[i])
+  # while i < array.length
+  # my_collect(collection) << yield(array[i])
     # yield(array[i])
     i += 1
   end
