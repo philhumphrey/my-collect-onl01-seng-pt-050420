903ebafa78e80baedcb6d6355ab1d71a594eed19
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 03:33:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@
   def my_collect(array)
     collection =[]
    i = 0
-  # while i < array.length
+  while i < array.length
   # my_collect(collection) << yield(array[i])
     # yield(array[i])
     i += 1
